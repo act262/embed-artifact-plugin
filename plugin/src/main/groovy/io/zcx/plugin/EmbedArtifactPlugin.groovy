6aa610071de87ff06c2cd7badca4d0349f419818
act262 <act262@gmail.com>
2019-03-10 17:39:08 +0800

    feat(plugin): support AGP3.3.2

--- plugin/src/main/groovy/io/zcx/plugin/EmbedArtifactPlugin.groovy
+++ plugin/src/main/groovy/io/zcx/plugin/EmbedArtifactPlugin.groovy
@@ -5,11 +5,13 @@ import com.android.build.gradle.api.LibraryVariant
 import com.android.build.gradle.internal.dsl.BuildType
 import com.android.build.gradle.tasks.InvokeManifestMerger
 import com.android.build.gradle.tasks.ManifestProcessorTask
+import com.android.builder.model.Version
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.ProjectConfigurationException
 import org.gradle.api.artifacts.ModuleVersionIdentifier
 import org.gradle.api.artifacts.ResolvedArtifact
+import org.gradle.util.VersionNumber
 
 /**
  * Process multiple artifact(aar、jar) merge into one artifact.
@@ -101,7 +103,18 @@ class EmbedArtifactPlugin implements Plugin<Project> {
         project.android.libraryVariants.all { LibraryVariant variant ->
             def variantName = variant.name.capitalize()
             ManifestProcessorTask processManifestTask = project.tasks["process${variantName}Manifest"]
-            def mainManifestFile = new File(processManifestTask.manifestOutputDirectory, "AndroidManifest.xml")
+            def mainManifestFile
+
+            def versionNumber = VersionNumber.parse(Version.ANDROID_GRADLE_PLUGIN_VERSION)
+            def ver_3_3_0 = VersionNumber.parse('3.3.0')
+            if (versionNumber >= ver_3_3_0) {
+                mainManifestFile = processManifestTask.manifestOutputFile.get().getAsFile() // Provider<RegularFile>
+            }else {
+                // 3.1.0 ~ 3.2.x
+                mainManifestFile = processManifestTask.manifestOutputFile // File
+            }
+
+
             def secondaryManifestFiles = Collections.singletonList(project.file("$explodedDir/AndroidManifest.xml"))
 
             InvokeManifestMerger manifestsMergeTask = project.tasks.create("merge${id.name.capitalize()}${variantName}Manifest", InvokeManifestMerger)
